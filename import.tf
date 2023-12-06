a8978da2220575c899a9a8f79c2276ceab546194
Srikaanth <129021608+Srikaanth62@users.noreply.github.com>
2023-12-06 22:04:33 +0530

    Update import.tf

--- import.tf
+++ import.tf
@@ -1,1 +1,12 @@
+# main.tf
+
+provider "aws" {
+  region = "us-east-1"
+  # Add other AWS provider configurations if needed
+}
+
+resource "aws_vpc" "wordpress-vpc" {
+  cidr_block = "10.0.0.0/16"  # Replace with your desired CIDR block
+  # Add other configuration options as needed
+}
 
